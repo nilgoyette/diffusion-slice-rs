dc5a7100fe3db0a79e139a2402c87939758e65ed
Nil Goyette <nil.goyette@imeka.ca>
2024-07-05 14:03:21 -0400

    Simple CLI with clap

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,27 @@ use std::{
     time::Instant,
 };
 
+use clap::Parser;
 use glam::{uvec2, UVec2};
 
 mod file;
 mod graphics;
 
 type Image = image::RgbaImage;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Input NiFTI image
+    pub input_image: PathBuf,
+
+    /// Output png image
+    pub output: PathBuf,
+
+    /// Width and height of the output 2D image
+    #[arg(num_args(2), long, default_values = ["800", "600"])]
+    pub output_size: Vec<u32>,
+}
 
 pub struct UserInputs {
     pub src_img: Image,
@@ -19,13 +34,16 @@ pub struct UserInputs {
 fn main() {
     let start = Instant::now();
 
+    let args = Args::parse();
+    println!("{args:?}");
+
     init_logger();
 
     // TODO Make this parameterable
     // TODO Validate user inputs
     let inputs = UserInputs {
         src_img: file::load_image(Path::new("sunshine.jpg")),
-        dst_img_size: uvec2(1200, 800),
+        dst_img_size: uvec2(args.output_size[0], args.output_size[1]),
         dst_img_path: PathBuf::from("output.png"),
     };
     let image = graphics::run_full_pipeline(&inputs);
