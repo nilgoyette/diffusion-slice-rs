a72301b815ff6049793d6950bb55413fb3e37808
Nil Goyette <nil.goyette@proton.me>
2024-07-12 22:23:06 -0400

    Pick name for output images

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct Args {
     /// Input NIfTI image
     pub input_image: PathBuf,
 
-    /// Output png image
+    /// Output folder to save all png
     pub output: PathBuf,
 
     /// Width and height of the output 2D image
@@ -28,7 +28,6 @@ struct Args {
 pub struct UserInputs {
     pub src_img: ImageSlice,
     pub dst_img_size: UVec2,
-    pub dst_img_path: PathBuf,
 }
 
 fn main() {
@@ -39,17 +38,18 @@ fn main() {
     println!("{args:?}");
 
     let (nifti_header, data) = read_3d_image::<_, f32>(args.input_image);
-    let slicer = Slicer::from_3d(nifti_header, data, 1, View::Anterior, (0.3, 0.7));
+    let slicer = Slicer::from_3d(nifti_header, data, 3, View::Anterior, (0.3, 0.7));
     for slice in slicer.slices {
         let inputs = UserInputs {
             src_img: slice.data,
             dst_img_size: uvec2(args.output_size[0], args.output_size[1]),
-            dst_img_path: PathBuf::from("output.png"),
         };
         let image = graphics::run_full_pipeline(&inputs);
 
-        // TODO Rename output image according to view and index
-        file::save_image(image, &inputs.dst_img_path);
+        // TODO Support a prefix, like "prefix{}_{}.png"
+        let mut write_to = args.output.clone();
+        write_to.push(format!("{}_{}.png", slice.view.name(), slice.index));
+        file::save_image(image, &write_to);
     }
 
     log::info!("Program duration: {:?}", start.elapsed());
